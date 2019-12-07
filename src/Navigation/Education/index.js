b4135b3bb2cab41b8252c4f81dec5022b78f5c42
ShilpiMaurya <meshilpi94@gmail.com>
2019-12-07 21:16:39 +0530

    styling and responsiveness done

--- src/Navigation/Education/index.js
+++ src/Navigation/Education/index.js
@@ -7,22 +7,20 @@ function Education(props, ref) {
   return (
     <div ref={ref} className="education">
       <EducationHeader heading="Education" />
-      <div>
-        <EducationContent
-          startdate={data.education[0].startDate}
-          enddate={data.education[0].endDate}
-          institution={data.education[0].institution}
-          area={data.education[0].area}
-          courses={data.education[0].courses[0]}
-        />
-        <EducationContent
-          startdate={data.education[1].startDate}
-          enddate={data.education[1].endDate}
-          institution={data.education[1].institution}
-          area={data.education[1].area}
-          courses={data.education[1].courses[0]}
-        />
-      </div>
+      <EducationContent
+        startdate={data.education[0].startDate}
+        enddate={data.education[0].endDate}
+        institution={data.education[0].institution}
+        area={data.education[0].area}
+        courses={data.education[0].courses[0]}
+      />
+      <EducationContent
+        startdate={data.education[1].startDate}
+        enddate={data.education[1].endDate}
+        institution={data.education[1].institution}
+        area={data.education[1].area}
+        courses={data.education[1].courses[0]}
+      />
     </div>
   );
 }

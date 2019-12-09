6eceac8d48926ff6059cf905518db08e70bd14e1
ShilpiMaurya <meshilpi94@gmail.com>
2019-12-09 20:23:03 +0530

    final draft

--- src/Navigation/Education/EducationContent.js
+++ src/Navigation/Education/EducationContent.js
@@ -1,26 +1,30 @@
 import React from "react";
 import "./index.css";
 function EducationContent(props) {
-  return (
-    <div className="education_container">
-      <div className="education_container_part1">
-        <div className="education_date">
-          {props.startdate}
-          <br />
-          {props.enddate}
+  return props.content.map(item => {
+    return item.map(({ startDate, endDate, institution, area, courses }) => {
+      return (
+        <div className="education_container">
+          <div className="education_container_part1">
+            <div className="education_date">
+              {startDate}
+              <br />
+              {endDate}
+            </div>
+            <div className="education_logo" />
+          </div>
+          <div className="vertical_line" />
+          <div className="education_container_part2">
+            <div className="education_type">
+              <div className="education_type_heading">{institution}</div>
+              <div className="education_type_sub-heading">{area}</div>
+            </div>
+            <div className="horizontal_line" />
+            <div className="education_description">{courses}</div>
+          </div>
         </div>
-        <div className="education_logo" />
-      </div>
-      <div className="vertical_line" />
-      <div className="education_container_part2">
-        <div className="education_type">
-          <div className="education_type_heading">{props.institution}</div>
-          <div className="education_type_sub-heading">{props.area}</div>
-        </div>
-        <div className="horizontal_line" />
-        <div className="education_description">{props.courses}</div>
-      </div>
-    </div>
-  );
+      );
+    });
+  });
 }
 export default EducationContent;
--- src/Navigation/Education/index.js
+++ src/Navigation/Education/index.js
@@ -7,20 +7,7 @@ function Education(props, ref) {
   return (
     <div ref={ref} className="education">
       <EducationHeader heading="Education" />
-      <EducationContent
-        startdate={data.education[0].startDate}
-        enddate={data.education[0].endDate}
-        institution={data.education[0].institution}
-        area={data.education[0].area}
-        courses={data.education[0].courses[0]}
-      />
-      <EducationContent
-        startdate={data.education[1].startDate}
-        enddate={data.education[1].endDate}
-        institution={data.education[1].institution}
-        area={data.education[1].area}
-        courses={data.education[1].courses[0]}
-      />
+      <EducationContent content={data.education} />
     </div>
   );
 }

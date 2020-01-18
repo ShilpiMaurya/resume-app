fcfaa14a9600f5208bae23e3b9483edd5f1ec69c
ShilpiMaurya <47273243+ShilpiMaurya@users.noreply.github.com>
2020-01-18 18:01:16 +0530

    feature/RA-7 (#12)
    * contact section
    
    * Feature/RA-6 (#11)
    
    * education structure done
    
    * styling 50% done
    
    * styling and responsiveness done
    
    * final draft
    
    * inhertance changes
    
    * some logos
    
    * key prop added
    
    * index prop changes
    
    * on scroll changes
    
    * on scrolling animation done correctly
    
    * education section done
    
    * contact section
    
    * some changes in styling
    
    * with better names
    
    * adding link tag

--- src/Navigation/Contact/index.js
+++ src/Navigation/Contact/index.js
@@ -1,10 +1,39 @@
 import React from "react";
 import "./index.css";
+import ContactSection from "./ContactSection";
 function Contact(props, ref) {
   return (
-    <h2 ref={ref} className="contact">
-      Contact
-    </h2>
+    <div ref={ref} className="contact">
+      <h1 className="contact_heading">Contact Me</h1>
+      <div className="contact_container">
+        <ContactSection
+          heading="Call Me"
+          subheading={
+            <a href={`tel:${props.contactData.phone}`}>
+              {props.contactData.phone}
+            </a>
+          }
+          imgsrc={require("../../images/phone-no1.png")}
+          picInfo="phone logo"
+        />
+        <ContactSection
+          heading="Mail Me"
+          subheading={
+            <a href={`mailto:${props.contactData.email}`}>
+              {props.contactData.email}
+            </a>
+          }
+          imgsrc={require("../../images/email1.png")}
+          picInfo="email logo"
+        />
+        <ContactSection
+          heading="Location"
+          subheading={props.contactData.location.country}
+          imgsrc={require("../../images/location1.png")}
+          picInfo="location logo"
+        />
+      </div>
+    </div>
   );
 }
 export default React.forwardRef(Contact);

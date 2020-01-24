aef7f973cb6bd0bee90a6ac393e2325dce85ada6
ShilpiMaurya <47273243+ShilpiMaurya@users.noreply.github.com>
2020-01-24 20:56:32 +0530

    feature/RA-8 (#13)
    * resume download page
    
    * included raw resume.json
    
    * resume download done

--- src/Navigation/Resumedownload/index.js
+++ src/Navigation/Resumedownload/index.js
@@ -1,9 +1,17 @@
 import React from "react";
+import "./index.css";
 function Resume(props, ref) {
   return (
-    <h2 ref={ref} className="resume-download">
-      Resume
-    </h2>
+    <div ref={ref} className="resume-download">
+      <div className="resume-download_logo"></div>
+      <a
+        className="resume-download_heading"
+        href="https://raw.githubusercontent.com/ShilpiMaurya/resume-app/develop/src/resume.json"
+        target={"_blank"}
+      >
+        Download Resume
+      </a>
+    </div>
   );
 }
 export default React.forwardRef(Resume);

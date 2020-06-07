140e88feaa31d25a5409531c5f20ca3988734fa4
ShilpiMaurya <meshilpi94@gmail.com>
2020-06-07 16:52:40 +0530

    improvised changes

--- src/Navigation/Skill/SkillContent.js
+++ src/Navigation/Skill/SkillContent.js
@@ -3,14 +3,14 @@ import "./index.css";
 function SkillContent(props) {
   return props.info.map((subSkills, index) => {
     return subSkills.map(({ name, level, logo }) => {
-      const imgSource = props.picture(`./${logo}`);
+      // const imgSource = props.picture(`./${logo}`);
       return (
         <div key={index} className="skill_language_items">
-          <img
+          {/* <img
             className="skill_images"
             src={imgSource}
             alt="logo of following skill"
-          />
+          /> */}
           <div className="skills_web-development_name">{name}</div>
           <div className="skills_web-development_level">{level}</div>
         </div>

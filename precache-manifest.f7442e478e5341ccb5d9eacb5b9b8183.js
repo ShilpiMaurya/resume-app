5216647565a8002ea8303a7ac6b9af88b52dcd2b
ShilpiMaurya <ShilpiMaurya@users.noreply.github.com>
2021-04-12 16:43:22 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.f7442e478e5341ccb5d9eacb5b9b8183.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0ef08eabbc74ad65f05f6b0189d3f4a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b1355481d229e2640cb3",
+    "url": "./static/css/main.76f00602.chunk.css"
+  },
+  {
+    "revision": "633f8798c251da40ed10",
+    "url": "./static/js/2.929c5b0e.chunk.js"
+  },
+  {
+    "revision": "b1355481d229e2640cb3",
+    "url": "./static/js/main.579457a0.chunk.js"
+  },
+  {
+    "revision": "46875ab56589fcb60b0a",
+    "url": "./static/js/runtime~main.40b47b02.js"
+  },
+  {
+    "revision": "d9ca4444a81e0f2654bd473f334c31c4",
+    "url": "./static/media/crop.d9ca4444.jpg"
+  },
+  {
+    "revision": "c737aef1b66965a999be34c762be2bf6",
+    "url": "./static/media/display-image.c737aef1.jpg"
+  },
+  {
+    "revision": "4791611409737c3cd7c645bfb9e18abf",
+    "url": "./static/media/display-pic.47916114.jpg"
+  },
+  {
+    "revision": "41b90ed2bce1c017bc0b21ba82578fd5",
+    "url": "./static/media/git.41b90ed2.png"
+  },
+  {
+    "revision": "2ca5d01c103c746c589cf2627d114ead",
+    "url": "./static/media/location.2ca5d01c.png"
+  },
+  {
+    "revision": "465219ca1fa3357d150ef5603bd7677f",
+    "url": "./static/media/location1.465219ca.png"
+  },
+  {
+    "revision": "1818a85d330d42501ce7da38d529e85d",
+    "url": "./static/media/logo.1818a85d.png"
+  },
+  {
+    "revision": "adecc50d8d14b68ee92e4614735bd792",
+    "url": "./static/media/profile.adecc50d.jpeg"
+  },
+  {
+    "revision": "7c1b3a294ded07f143ed7469c38d63bc",
+    "url": "./static/media/seo.7c1b3a29.png"
+  },
+  {
+    "revision": "88fb5e96fcb8354aca158fcae17461d5",
+    "url": "./static/media/shilpi.88fb5e96.jpg"
+  },
+  {
+    "revision": "c8cdff14544777070705ddea0895a57a",
+    "url": "./static/media/shilpi2.c8cdff14.jpg"
+  },
+  {
+    "revision": "0df72deba9d573f12e217bb59df35db3",
+    "url": "./static/media/skill-background.0df72deb.jpg"
+  },
+  {
+    "revision": "3c6191c774eea7ba20b9079b0e45f3bd",
+    "url": "./static/media/skill_logo.3c6191c7.png"
+  }
+]);
